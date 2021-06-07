8739eecbcf6fb6f744fbb00ea1c4af7992d29bf1
Amit Kumar <amit557@gmail.com>
2021-06-07 21:24:15 +0530

    updated script

--- gencsv.sh
+++ gencsv.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "0, 234" > inputFile
+echo "0, 24" > inputFile
 echo "1, 98" >> inputFile
 echo "2, 34" >> inputFile
 echo "3, 23" >> inputFile
+echo "4, 33" >> inputFile
+echo "5, 43" >> inputFile
+echo "6, 53" >> inputFile
+echo "7, 63" >> inputFile
+echo "8, 73" >> inputFile
+echo "9, 83" >> inputFile
